7abd18148f1a5e9e200f7ec531f67ce0d98bc239
torgodly <torgodly@gmail.com>
2025-07-16 19:17:02 +0300

    Add Course model, factory, and migration files; update composer dependencies

---
+++ database/migrations/2025_07_16_161308_create_courses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('trainer_id')->constrained('users');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('slug')->unique();
+            $table->json('learn_goals')->nullable();
+            $table->json('requirements')->nullable();
+            $table->enum('level', ['beginner', 'intermediate', 'advanced'])->default('beginner');
+            $table->decimal('price', 8, 2)->default(0.00);
+            $table->boolean('is_approved')->default(false);
+            $table->boolean('is_published')->default(false);
+            $table->boolean('is_free')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
